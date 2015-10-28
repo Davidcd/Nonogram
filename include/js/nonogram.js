d7003ba88c9b912ad49e2baeb57b63e656c8d900
David Dec <daviddec.exe@gmail.com>
2015-10-27 22:17:49 -0400

    Added row labels
    On creation, the gameboard is now parsed row by row and a label is added
    for each.

--- include/js/nonogram.js
+++ include/js/nonogram.js
@@ -10,34 +10,63 @@ function makeGameBoard(boardSize) {
 		line += "</tr>";
 		$("#gameBoard").append(line);
 	}
-	populateRows(createRows(5, 5));
+	var answerSheet = createRows(boardSize,boardSize);
+	populateRows(answerSheet);
+	generateLabels(answerSheet);
 }
 
 //Return a multidimensional array of booleans with true representing cells that must be "on"
 //	and false representing cells that must be "off" 
 function createRows(numRows, rowLength) {
-	var rows = new Array(new Array());
+	var count;
+	var countString;
+	var rows = [];
+
 	for(i = 0; i < numRows; i++) {
+		count = 0;
+		countString = "";
 		rows.push(new Array());
 		for(n = 0; n < rowLength; n++) {
 			if(Math.random() > 0.5)
 				rows[i].push(true);
-			else
+			else 
 				rows[i].push(false);
 		}
 	}
 	return rows;
 }
 
-//Takes a multidimensional array, parses it, and modifies the gameboard accordingly
+//Takes a 2D array, parses it, and populates the solved gameboard to make it easier to debug
 function populateRows(rows) {
 	for(i = 0; i < rows.length; i++) {
 		for(n = 0; n < rows[i].length; n++){
 			var cell = "#c_" + i + "_" + n;
-			//console.log("Cell: " + cell + " Value: " + rows[i][n]);
 			if(rows[i][n] == true)
-				//$(cell).addClass("on");
-			$(cell).append('<div class="on"/>');
+				$(cell).append('<div class="on"/>');
 		}
+	}
+}
+
+function generateLabels(rows) {
+	var label;
+	var labelCount;
+
+	for(i = 0; i < rows.length; i++) {
+		label = "";
+		labelCount = 0;
+		for(n = 0; n < rows[i].length; n++){
+			var cell = "#c_" + i + "_" + n;
+			if(rows[i][n] == true)
+				labelCount++;
+			else if(labelCount > 0){
+				label += labelCount;
+				label += " ";
+				labelCount = 0;
+			}
+		}
+		if(labelCount > 0) 
+			label += labelCount;
+			
+		$(cell).parent().prepend("<td>" + label + "</td>");
 	}
 }
