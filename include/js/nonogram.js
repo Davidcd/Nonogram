a68e3eb58eca6c1254af429555cd3057d3af850c
Davidcd <decdavid@ymail.com>
2016-01-01 21:04:33 -0500

    Fixed a bug where rows would be generated without any cells on.

--- include/js/nonogram.js
+++ include/js/nonogram.js
@@ -15,6 +15,7 @@ function makeGameBoard(boardSize) {
 	}
 	size = boardSize;
 	answerSheet = createRows(boardSize,boardSize);
+	answerSheet = checkEmpty(answerSheet); //Check if any rows or columns do not have any entries in them
 	//populateRows(answerSheet);
 	generateLabels(answerSheet);
 }
@@ -29,15 +30,52 @@ function createRows(numRows, rowLength) {
 	for(i = 0; i < numRows; i++) {
 		count = 0;
 		countString = "";
-		rows.push(new Array());
+
+		//Create a new row, and then randomly add an on or off cell up to the length of the row
+		rows.push(new Array()); 
 		for(n = 0; n < rowLength; n++) {
-			if(Math.random() > 0.5)
+			if(Math.random() > 0.5) 
 				rows[i].push(true);
 			else 
 				rows[i].push(false);
 		}
 	}
 	return rows;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+//Checks to see if any rows or columns are empty and fixes it if that's the case
+function checkEmpty(answerSheet) {
+	var numOn;
+	//Check for empty rows
+	for(i = 0; i < answerSheet.length; i++) {
+		numOn = 0;
+		for(n = 0; n < answerSheet[i].length; n++) {
+			if(answerSheet[i][n]) 
+				numOn++;
+		}
+		if(numOn < 1) {
+			chosenOne = getRandomInt(0, answerSheet[i].length - 1);
+			answerSheet[i][chosenOne] = true;
+		}
+	}
+	//Check for empty columns
+	for(n = 0; n < answerSheet.length; n++) {
+		numOn = 0;
+		for(i = 0; i < answerSheet[n].length; i++) {
+			if(answerSheet[n][i]) 
+				numOn++;
+		}
+		if(numOn < 1) {
+			chosenOne = getRandomInt(0, answerSheet.length - 1);
+			answerSheet[n][chosenOne] = true;
+		}
+	}
+
+	return answerSheet;
 }
 
 //Takes a 2D array, parses it, and populates the solved gameboard to make it easier to debug
